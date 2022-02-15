7c9b8f016ce1d4a809f236441829581c20c82256
shitalkevadia <98792394+shitalkevadia@users.noreply.github.com>
2022-02-14 16:02:47 -1000

    1st commit

--- src/main/java/variousConsepts/gitTest.java
+++ src/main/java/variousConsepts/gitTest.java
@@ -1,5 +1,17 @@
 package variousConsepts;
 
 public class gitTest {
-
+	public static void main (String[] args) {
+		System.out.println("git test");
+		printMe();
+		printMe1();
+	}
+	
+	public static void printMe() {
+		System.out.println("get test1");
+	}
+	
+	public static void printMe1() {
+		System.out.println("get test1");
+}
 }
